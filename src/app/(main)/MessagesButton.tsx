022628a5b5e849a1ac4ad77ed20467fbea1ad67b
pcmi <pcminfanta@gmail.com>
2024-12-03 21:58:56 +0800

    Update MessagesButton.tsx

--- src/app/(main)/MessagesButton.tsx
+++ src/app/(main)/MessagesButton.tsx
@@ -23,7 +23,7 @@ export default function MessagesButton({ initialState }: MessagesButtonProps) {
   return (
     <Button
       variant="ghost"
-      className="flex items-center justify-start gap-3"
+      className="hidden lg:flex !important items-center justify-start gap-3"
       title="Messages"
       asChild
     >
