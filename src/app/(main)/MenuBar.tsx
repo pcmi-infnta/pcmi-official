a6d02755e63a632698442ac5b7940b178b21c63d
pcmi <pcminfanta@gmail.com>
2024-12-04 01:19:17 +0800

    Update MenuBar.tsx

--- src/app/(main)/MenuBar.tsx
+++ src/app/(main)/MenuBar.tsx
@@ -43,16 +43,16 @@ export default async function MenuBar({ className }: MenuBarProps) {
 
 
       <Button
-        variant="ghost"
-        className="flex items-center justify-start gap-3"
-        title="People"
-        asChild
-      >
-        <Link href="/people">
-          <Users />
-          <span className="hidden lg:inline">People</span>
-        </Link>
-      </Button>
+  variant="ghost"
+  className="flex items-center justify-start gap-3"
+  title="People"
+  asChild
+>
+  <Link href="/people">
+    <Users className="h-5 w-5" />
+    <span className="hidden lg:inline">People</span>
+  </Link>
+</Button>
 
 
 
