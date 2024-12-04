e6512de859af900fa503ab865a8650c154d67325
pcmi <pcminfanta@gmail.com>
2024-12-04 16:39:59 +0800

    Update BookmarkButton.tsx

--- src/components/posts/BookmarkButton.tsx
+++ src/components/posts/BookmarkButton.tsx
@@ -40,7 +40,7 @@ export default function BookmarkButton({
         : kyInstance.post(`/api/posts/${postId}/bookmark`),
     onMutate: async () => {
       toast({
-        description: `Post ${data.isBookmarkedByUser ? "un" : ""}bookmarked`,
+        description: `Post ${data.isBookmarkedByUser ? "un" : ""}saved`,
       });
 
       await queryClient.cancelQueries({ queryKey });
