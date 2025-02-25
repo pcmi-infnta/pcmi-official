ad9a8e0f286bcc71c56e40839ff498f458b40326
pcmi-infanta <pcminfanta@gmail.com>
2025-02-26 00:05:48 +0800

    Auto commit

--- src/app/api/posts/videos/route.ts
+++ src/app/api/posts/videos/route.ts
@@ -1,5 +1,6 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
+import { getPostDataInclude, PostsPage } from "@/lib/types";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -15,53 +16,28 @@ export async function GET(request: NextRequest) {
   const limit = 10;
 
   const posts = await prisma.post.findMany({
-  where: {
-    attachments: {
-      some: {
-        type: "VIDEO",
-      },
-    },
-  },
-  include: {
-    user: true,
-    attachments: true,
-    _count: {
-      select: {
-        comments: true,
-        likes: true,
-      },
-    },
-    likes: {
-      where: {
-        userId: user.id,
-      },
-      select: {
-        userId: true,  // Select userId instead of id
-        postId: true,  // You can also select postId if needed
-      },
-    },
-    bookmarks: {
-      where: {
-        userId: user.id,
-      },
-      select: {
-        id: true,  // This is fine since Bookmark has an id field
-      },
-    },
-  },
-  orderBy: {
-    createdAt: "desc",
-  },
-  take: limit + 1,
-  ...(cursor
-    ? {
-        cursor: {
-          id: cursor,
+    where: {
+      // Only fetch posts that have videos
+      attachments: {
+        some: {
+          type: "VIDEO",
         },
-        skip: 1,
-      }
-    : {}),
-});
+      },
+    },
+    include: getPostDataInclude(user.id),
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: limit + 1,
+    ...(cursor
+      ? {
+          cursor: {
+            id: cursor,
+          },
+          skip: 1,
+        }
+      : {}),
+  });
 
   let nextCursor: string | null = null;
   if (posts.length > limit) {
